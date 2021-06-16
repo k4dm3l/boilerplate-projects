6e07e06f1a67409f21ca73d7ab1131042f12bde2
Camilo Acevedo <camilo-mac@Camilos-MacBook-Pro.local>
2021-06-15 20:01:12 -0500

    Added base file class for auth0 managment process

--- src/providers/auth0-api/auth0Integration.ts
+++ src/providers/auth0-api/auth0Integration.ts
@@ -1,3 +1,10 @@
+import axios from 'axios';
+import request from 'request';
+import qs from 'query-string';
+import util from 'util';
+
+const requestAsync = util.promisify(request);
+
 export default class Auth0Integration {
   constructor() {}
 }
